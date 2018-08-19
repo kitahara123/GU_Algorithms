473d2c36af65fd29640b2b17f8ef942726ba5741
kit <kitahara@inbox.ru>
2018-08-19 11:06:38 +0300

    1 commit

---
+++ GU_Algorithms/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GU_Algorithms
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("hello world");
+		}
+	}
+}
