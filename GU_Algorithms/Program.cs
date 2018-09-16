dc8e36b499c5609aba01a678e6a8366244599e07
kit <kitahara@inbox.ru>
2018-09-16 14:51:35 +0300

    L4

--- GU_Algorithms/Program.cs
+++ GU_Algorithms/Program.cs
@@ -12,106 +12,64 @@ namespace GU_Algorithms
 
 	class Program
 	{
+		// 2. Решить задачу о нахождении длины максимальной последовательности с помощью матрицы.
+		// Я не смог разобраться как перевернуть матрицу так же как в методичке
+		static char[] a = new char[] { 'a', 'l', 'l', 'y', 'o', 'u', 'b', 'a', 's', 't', 'a', 'r', 'd', 's' };
+		static char[] b = new char[] { 'g', 'e', 't', 'f', 'i', 'r', 'e', 'd', 'u', 'p' };
+
+		//static char[] a = new char[] { 'g', 'e', 'e', 'k', 'b', 'r', 'a', 'i', 'n', 's' };
+		//static char[] b = new char[] { 'g', 'e', 'e', 'k', 'm', 'i', 'n', 'd', 's' };
+		static int[,] ab = new int[a.Length, b.Length];
+
 		static void Main(string[] args)
 		{
-			int[] arr = new int[] { 1, 10, 23, 2, 31, 3, 5, 3, 2, 7, 8, 8, 123, 345, 3324, 12, 23, 5, 23, 3, 43, 4, 2, 346, 7, 56, 3, 4, 22, 57 };
-			int[] arr1 = arr.ToArray();
-			int[] arr2 = arr.ToArray();
-			Console.WriteLine("Пузырьковая сортировка: " + BubbleSort(arr));
-			Console.WriteLine("Улучшенная пузырьковая сортировка: " + BubbleSortImproved(arr1));
-			Console.WriteLine("Сортировка вставками: " + InsertSort(arr2));
+			Console.WriteLine(lcs_length(0, 0));
 
-			foreach (int i in arr1)
+			Console.WriteLine();
+
+			Console.Write("  ");
+			foreach (char i in a)
 				Console.Write(i + " ");
 
 			Console.WriteLine();
-			Console.WriteLine("Элемент найден на индексе: " + BinarySearch(23, arr));
+
+			for (int i = 0; i < ab.GetLength(1); i++)
+			{
+				Console.Write(b[i] + " ");
+				for (int j = 0; j < ab.GetLength(0); j++)
+				{
+					Console.Write(ab[j, i] + " ");
+
+				}
+				Console.WriteLine();
+			}
 
 			Console.ReadKey();
 		}
 
-		static int BubbleSort(int[] arr)
+		static int lcs_length(int i, int j)
 		{
-
-			int counter = 0;
-			for (int i = 0; i < arr.Length; i++)
-				for (int j = 0; j < arr.Length - 1; j++)
-				{
-					counter++;
-					if (arr[j] > arr[j + 1])
-					{
-						var t = arr[j];
-						arr[j] = arr[j + 1];
-						arr[j + 1] = t;
-					}
-				}
-			return counter;
-		}
-
-		// 1. Попробовать оптимизировать пузырьковую сортировку. Сравнить количество операций сравнения оптимизированной и не оптимизированной программы. 
-		//Написать функции сортировки, которые возвращают количество операций.
-		static int BubbleSortImproved(int[] arr)
-		{
-			int counter = 0;
-			int flag = 0;
-			for (int i = 0; i < arr.Length; i++)
+			int res = 0;
+			if (a.Length <= i || b.Length <= j)
 			{
-				flag = 0;
-				for (int j = 0; j < arr.Length - 1; j++)
-				{
-					counter++;
-					if (arr[j] > arr[j + 1])
-					{
-						flag = 1;
-						var t = arr[j];
-						arr[j] = arr[j + 1];
-						arr[j + 1] = t;
-					}
-				}
-				if (flag == 0) return counter;
+				return 0;
 			}
-			return counter;
-		}
-
-		// 3. Реализовать бинарный алгоритм поиска в виде функции, которой передается отсортированный массив. Функция возвращает индекс найденного элемента или -1, если элемент не найден.
-		static int BinarySearch(int num, int[] arr)
-		{
-			int L = 0, R = arr.Length - 1;
-			while (L <= R)
+			else if (a[i] == b[j])
 			{
-				int m = (L + R) / 2;
-
-				if (arr[m] < num)
-					L = m + 1;
-				else
-					R = m - 1;
-				if (arr[m] == num) return m;
+				res = 1 + lcs_length(i + 1, j + 1);
 			}
-			return -1;
-
-		}
-
-		static int InsertSort(int[] a)
-		{
-			int counter = 0;
-			for (int i = 0; i < a.Length; i++)
+			else
 			{
-				int t = a[i];
-				int j = i;
-				while (j > 0 && a[j - 1] > t)
-				{
-					counter++;
-					var temp = a[j];
-					a[j] = a[j - 1];
-					a[j - 1] = temp;
-					j--;
-				}
+				res = max(lcs_length(i + 1, j), lcs_length(i, j + 1));
 			}
-			return counter;
+
+			ab[i, j] = res;
+			return res;
 		}
-
-
-
+		static int max(int a, int b)
+		{
+			return a > b ? a : b;
+		}
 	}
 
 
