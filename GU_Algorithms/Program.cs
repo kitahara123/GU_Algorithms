58ad091fbe267f7f364357997b7b6b841a3bdddd
kit <kitahara@inbox.ru>
2018-09-23 14:53:58 +0300

    бранч 6 урока

--- GU_Algorithms/Program.cs
+++ GU_Algorithms/Program.cs
@@ -14,109 +14,14 @@ namespace GU_Algorithms
 
 	class Program
 	{
-		// 5 урок
+		// 6 урок
 		static void Main(string[] args)
 		{
-			#region 6. *Реализовать очередь.
-			Queue q = new Queue();
-			q.Enqueue(new QueueNode("1"));
-			q.Enqueue(new QueueNode("2"));
-			q.Enqueue(new QueueNode("3"));
-			q.Enqueue(new QueueNode("4"));
-			q.Enqueue(new QueueNode("5"));
-
-			for (int i = 0; i < q.Length;)
-			{
-				Console.WriteLine(q.Dequeue().Value);
-			}
-			#endregion
-
-
-			Console.ReadKey();
-		}
-	}
-
-	// 6. *Реализовать очередь.
-	class Queue
-	{
-		private int length;
-		private QueueNode tail;
-		private QueueNode head;
-
-		public int Length { get => length; set => length = value; }
-
-		internal Queue()
-		{
-			length = 0;
-			tail = null;
-			head = null;
 		}
 
-		public void Enqueue(QueueNode n)
-		{
-			if(head == null)
-			{
-				head = n;
-				tail = n;
-				length = 1;
-				return;
-			}
 
-			tail.Next = n;
-			tail = n;
-			length++;
-		}
-		public QueueNode Dequeue()
-		{
-			if (head == null) throw new NoSuchElementException();
 
-			var h = head;
-			head = head.Next;
-			length--;
-			return h;
-		}
 
 	}
-
-	class QueueNode
-	{
-		private QueueNode next;
-		private string value;
-
-		public string Value { get => value; set => this.value = value; }
-		public QueueNode Next { get => next; set => next = value; }
-
-		internal QueueNode(string value)
-		{
-			this.value = value;
-
-		}
-
-	}
-
-	[Serializable]
-	internal class NoSuchElementException : Exception
-	{
-		public NoSuchElementException()
-		{
-		}
-
-		public NoSuchElementException(string message) : base(message)
-		{
-		}
-
-		public NoSuchElementException(string message, Exception innerException) : base(message, innerException)
-		{
-		}
-
-		protected NoSuchElementException(SerializationInfo info, StreamingContext context) : base(info, context)
-		{
-		}
-	}
-
-
-
-
-
 }
 
