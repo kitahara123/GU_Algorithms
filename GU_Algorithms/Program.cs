a0c332b0bc4cf4dc5d560cb87e9019ef4906417e
kit <kitahara@inbox.ru>
2018-10-06 17:35:48 +0300

    d

--- GU_Algorithms/Program.cs
+++ GU_Algorithms/Program.cs
@@ -17,9 +17,57 @@ namespace GU_Algorithms
         // 7 урок
         static void Main(string[] args)
         {
+            int[,] graph = new int[7,7];
 
+
+            graph[0, 1] = 5;
+            graph[0, 6] = 9;
+
+            graph[1, 0] = 5;
+            graph[1, 2] = 8;
+            graph[1, 6] = 2;
+
+            graph[2, 1] = 8;
+            graph[2, 5] = 4;
+
+            graph[3, 5] = 3;
+
+            graph[4, 5] = 1;
+
+            graph[5, 2] = 4;
+            graph[5, 3] = 3;
+            graph[5, 4] = 1;
+            graph[5, 6] = 3;
+
+            graph[6, 0] = 9;
+            graph[6, 1] = 2;
+            graph[6, 5] = 6;
+
+            Console.ReadKey();
         }
 
+
+        int Dijkstra(int from, int to, int[,] graph)
+        {
+            int[] checkd = new int[7];
+            int[] route = new int[7] { int.MaxValue, int.MaxValue, int.MaxValue, int.MaxValue, int.MaxValue, int.MaxValue, int.MaxValue };
+            Queue<int> queue = new Queue<int>();
+
+            route[from] = 0;
+            for (int i = 0; i < graph.GetLength(0); i++)
+            {
+                var p = graph[i, from];
+                if (p > 0)
+                {
+                    queue.Enqueue(i);
+                    checkd[i] = 1;
+                    route[i] = p;
+                }
+            }
+            
+            
+
+        }
     }
 }
 
